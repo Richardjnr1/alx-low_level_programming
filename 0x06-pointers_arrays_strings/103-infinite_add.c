3fa7809416c34b1d0a310b023d78ebbefb658fec
Richardjnr1 <zackky1@hotmail.com>
2023-07-30 01:06:17 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * infinite_add -A function that adds two numbers
+ * @n1: variable 1
+ * @n2: variable 2
+ * @r: is the buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Return: 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_buffer -A function that prints a buffer
+ *
+ *
+ * Return:
+ */
+void print_buffer(char *b, int size)
+{
